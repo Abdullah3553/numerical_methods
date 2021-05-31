cef644dd90882328ac8d9d4cd4eb3b6e9e68d764
Abdullah3553 <abdullah.ayoub2000@gmail.com>
2021-05-31 12:12:09 +0200

    Octal > all added

--- Number_operations.h
+++ Number_operations.h
@@ -1,25 +1,45 @@
 #include "Numbers.h"
-#include <algorithm>
-
+#include <map>
 class Number_Operations{
+    static const int encoding_size = 16;
+
     int pow(int base, int power){
-        int return_val=1;
+        int return_num=1;
         for(int i=0;i<power;i++)
-            return_val*= base;
-        return return_val;
+            return_num*= base;
+        return return_num;
     }
-    char encoding[16]={'0', '1', '2', '3', '4', '5',
+
+//    int decoding(char encoded_character){
+//        int return_num=0;
+//        for(int i=0;i<encoding_size;i++){
+//            if(encoding[i]==encoded_character){
+//                return_num=i;
+//                break;
+//            }
+//        }
+//        return return_num;
+//    }
+
+    char encoding[encoding_size]={'0', '1', '2', '3', '4', '5',
                           '6', '7', '8', '9', 'A', 'B',
                           'C', 'D', 'E', 'F'};
+    map<char, int> decoding = {
+            {'0', 0},{'1', 1},{'2', 2},{'3', 3},
+            {'4', 4},{'5', 5}, {'6', 6},{'7', 7},
+            {'8', 8},{'9', 9},{'A', 10},{'B', 11},
+            {'C', 12},{'D', 13},{'E', 14},{'F', 15}
+    };
 public:
     // Binary >> all --Begin
+
     int binary_to_decimal(Binary number){
-        int return_val = 0;
-        for(int i=0;i<number.size();i++){
+        int return_num = 0;
+        for(int i=number.size()-1, j=0;i>-1;i--,j++){
             if(number[i]=='1')
-                return_val += pow(2, i);
+                return_num += pow(2, j);
         }
-        return return_val;
+        return return_num;
     }
 
     string binary_to_octal(Binary number){
@@ -58,14 +78,47 @@ public:
     }
 
     // Binary >> all --End
+//------------------------------//
+    // Decimal >> all --Begin
+
     string decimal_to_binary(Decimal number){
         string return_val;
         while(number.get()!=0){
             return_val += (char)((number.get()%2)+'0');
             number.set(number.get()/2);
         }
-        return return_val;
+        return string(return_val.rbegin(), return_val.rend());
     }
+
+    string decimal_to_octal(Decimal number){
+        return binary_to_octal(decimal_to_binary(number));
+    }
+
+    string decimal_to_hexadecimal(Decimal number){
+        return binary_to_hexadecimal(decimal_to_binary(number));
+    }
+    // Decimal >> all --End
+//------------------------------//
+    // Octal >> all --Begin
+
+    string octal_to_binary(Octal number){
+        return decimal_to_binary(octal_to_decimal(number));
+    }
+
+    int octal_to_decimal(Octal number){
+        int return_num=0;
+        for(int i=number.size()-1, j=0;i>-1;i--,j++){
+            return_num+=(pow(8, j) * decoding[number[i]]);
+        }
+        return return_num;
+    }
+
+    string octal_to_hexadecimal(Octal number){
+        return binary_to_hexadecimal(octal_to_binary(number));
+    }
+
+    // Octal >> all --End
+//------------------------------//
 
 
 };
--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main(){
     Number_Operations op;
+    // cases for testing
+    Octal x("14");
+    cout << op.octal_to_binary(x) << endl;
+    cout << op.octal_to_decimal(x) << endl;
+    cout << op.octal_to_hexadecimal(x) << endl;
+    return 0;
+}
+
+/*
     Binary x("1111");
 
     cout <<"Binary -> " << x.get() << " original\n";
@@ -12,5 +21,4 @@ int main(){
     cout << "Decimal -> "<<op.binary_to_decimal(x) << endl;
     cout << "Octal -> "<<op.binary_to_octal(x) << endl;
     cout << "Hexadecimal -> "<<op.binary_to_hexadecimal(x)<<endl;
-    return 0;
-}
+ * */
