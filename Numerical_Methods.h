d725c1a14b1ba0849c5360beb15b83059a087286
Abdullah <abdullah.ayoub2000@gmail.com>
2021-06-06 07:47:58 +0200

    little implementation

--- Numerical_Methods.h
+++ Numerical_Methods.h
@@ -6,27 +6,34 @@ using namespace std;
 
 class curve_fitting{
 protected:
-    const int table_size=100;
+    const int max_table_size=100;
+    int table_size;
     double table[2][table_size], searing_val;
 public:
     curve_fitting(){
-        int inner_size=0;
-        cout << "How many x's do you want ? (Maximum is " << table_size << ") ";
-        cin >> inner_size;
+        cout << "How many x's do you want ? (Maximum is " << max_table_size << ") ";
+        cin >> table_size;
         cout << "Enter x's : \n";
-        for(int i=0;i<inner_size;i++)
-            cin >> table_size[0][i];
+        for(int i=0;i<table_size;i++)
+            cin >> table[0][i];
 
         cout << "Enter f(x) : \n";
-        for(int i=0;i<inner_size;i++)
-            cin >> table_size[1][i];
+        for(int i=0;i<table_size;i++)
+            cin >> table[1][i];
 
         cout << "Enter the value you want to check : ";
         cin >> searing_val;
 
     }
     double apply_method(){
-        // the actual function
+        double x_sum=0, y_sum=0, xMuly_sum=0, xMulx_sum,=0;
+        for(int i=0;i<table_size;i++){
+            x_sum += table[0][i];
+            y_sum += table[1][i];
+            xMulx_sum += (x*x);
+            xMuly_sum += (x*y);
+        }
+
     };
 };
 
